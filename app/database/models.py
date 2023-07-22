b5f3380ac974fd83421f96154c3f37ae8246ac3c
evggor87 <evggor87@gmail.com>
2023-07-22 15:40:23 +0300

    0.0.2
    Изменены модели

--- app/database/models.py
+++ app/database/models.py
@@ -1,11 +1,12 @@
+from datetime import datetime
+from turtle import back
 from sqlalchemy import ForeignKey, String, create_engine, ForeignKey
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
-from app.config import SQLALCHEMY_URL, SQLALCHEMY_ECHO
-
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column, relationship
+from config import SQLALCHEMY_URL, SQLALCHEMY_ECHO
 
 engine = create_engine(url=SQLALCHEMY_URL, echo=SQLALCHEMY_ECHO)
 Session = sessionmaker(engine)
-session = Session()
+
 
 class Base(DeclarativeBase):
     pass
@@ -18,12 +19,23 @@ class User(Base):
     tg_id: Mapped[int]
     name: Mapped[str] = mapped_column(String(32))
 
+    orders: Mapped['Order'] = relationship(back_populates='user')
+
+
+class Banned(Base):
+    __tablename__ = 'banned'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id: Mapped[int]
+
 
 class Platform(Base):
     __tablename__ = 'platforms'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(32))
+
+    consoles: Mapped['Console'] = relationship(back_populates='platform')
 
 
 class Console(Base):
@@ -33,6 +45,10 @@ class Console(Base):
     name: Mapped[str] = mapped_column(String(32))
     platform_id: Mapped[int] = mapped_column(ForeignKey('platforms.id'))
 
+    platform: Mapped['Platform'] = relationship(back_populates='consoles')
+    regions: Mapped['Region'] = relationship(back_populates='console')
+    games: Mapped['Game'] = relationship(back_populates='console')
+
 
 class Region(Base):
     __tablename__ = 'regions'
@@ -40,6 +56,9 @@ class Region(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(32))
     console_id: Mapped[int] = mapped_column(ForeignKey('consoles.id'))
+
+    console: Mapped['Console'] = relationship(back_populates='regions')
+    games: Mapped['Game'] = relationship(back_populates='region')
 
 
 class Game(Base):
@@ -54,6 +73,23 @@ class Game(Base):
     for_sale: Mapped[bool]
     desc: Mapped[str]
 
+    region: Mapped['Region'] = relationship(back_populates='games')
+    console: Mapped['Console'] = relationship(back_populates='games')
+    order: Mapped['Order'] = relationship(back_populates='game')
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    order_num: Mapped[str] = mapped_column(String(12))
+    date: Mapped[datetime]
+    state: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    game_id: Mapped[int] = mapped_column(ForeignKey('games.id'))
+
+    user: Mapped['User'] = relationship(back_populates='orders')
+    game: Mapped['Game'] = relationship(back_populates='order')
 
 
 async def create_base():
--- run.py
+++ run.py
@@ -3,20 +3,24 @@ import logging
 
 from aiogram import Bot, Dispatcher
 
-from app.config import BOT_TOKEN
+from config import TOKEN
 from app.handlers.user import router
 from app.handlers.admin import admin
 from app.database.models import create_base
 
+
 async def main():
 
-    logging.basicConfig(level=logging.INFO)
     await create_base()
-    bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
+    bot = Bot(token=TOKEN, parse_mode='HTML')
     dp = Dispatcher()
     dp.include_routers(router, admin)
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
